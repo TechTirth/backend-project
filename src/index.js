628b7872dab11c28e5973426d852dd2e9a813e98
Tirth Vanparia <144980949+TechTirth@users.noreply.github.com>
2024-06-01 15:40:12 +0530

    Connecting MongoDB (Two ways)

---
+++ src/index.js
@@ -0,0 +1,33 @@
+//require('dotenv').config({path: './env'});  you want env vars to load at the start itself
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: "./env" // see the changes in package.json files
+})
+
+connectDB()
+
+
+
+//JUST ANOTHER WAY TO CONNECT DB P.S -> NOT INDUSTRY FRIENDLY
+/*
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("ERRR: ",error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`The app is listening on ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+
+*/
